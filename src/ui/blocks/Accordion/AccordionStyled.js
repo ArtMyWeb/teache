a5b8b1a12dc923dcb3aea6dea8883c6e058a93ac
Yiriy Vasiliev <yuriy.vasiliev@artmyweb.com>
2022-01-13 18:20:51 +0200

    added responsive styles for accordion

--- src/ui/blocks/Accordion/AccordionStyled.js
+++ src/ui/blocks/Accordion/AccordionStyled.js
@@ -7,6 +7,12 @@ export const AccordionWrapper = styled.div`
   @media (max-width: 1024px) {
     padding: 0 40px 31px 40px;
   }
+  @media (max-width: 768px) {
+    padding: 0 20px 31px 20px;
+  }
+  @media (max-width: 640px) {
+    padding: 0 7px 26px 7px;
+  }
 `;
 
 export const Title = styled.div`
@@ -17,12 +23,27 @@ export const Title = styled.div`
   @media (max-width: 1024px) {
     margin-bottom: 10px;
   }
+  @media (max-width: 768px) {
+    font-size: 26px;
+    text-align: center;
+  }
+  @media (max-width: 640px) {
+    font-size: 16px;
+    text-align: center;
+    font-weight: bold;
+  }
 `;
 
 export const AccordionStyled = styled.div`
   width: 100%;
   height: 100%;
   margin: 68px auto 0;
+  @media (max-width: 1024px) {
+    margin: 40px auto 0;
+  }
+  @media (max-width: 768px) {
+    margin: 20px auto 0;
+  }
 `;
 
 export const Header = styled.div`
@@ -57,6 +78,17 @@ export const Tab = styled.div`
   &:hover {
     color: ${themeColor("blue")};
   }
+  @media (max-width: 1024px) {
+    width: 170px;
+    padding: 14px;
+    font-size: 14px;
+  }
+
+  @media (max-width: 768px) {
+    width: 120px;
+    padding: 12px;
+    font-size: 12px;
+  }
 `;
 
 export const AccordionItem = styled.div`
@@ -67,6 +99,9 @@ export const AccordionItem = styled.div`
   padding: 30px;
   border: 2px solid ${themeColor("lightBlue2")};
   border-radius: 10px;
+  @media (max-width: 768px) {
+    padding: 10px 15px;
+  }
 `;
 
 export const AccordionTitle = styled.div`
@@ -79,11 +114,17 @@ export const AccordionTitle = styled.div`
   @media (max-width: 1024px) {
     font-size: 16px;
   }
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
 `;
 
 export const AccordionBody = styled.div`
   display: ${({ isOpen }) => (isOpen ? "block" : "none")};
   padding-right: 60px;
+  @media (max-width: 768px) {
+    padding-right: 0;
+  }
 `;
 
 export const AccordionText = styled.div`
@@ -92,6 +133,11 @@ export const AccordionText = styled.div`
   font-family: Montserrat, sans-serif;
   font-size: ${themeFontSize("medium")};
   line-height: 27px;
+  @media (max-width: 768px) {
+    font-size: 12px;
+    margin-top: 11px;
+    line-height: 18px;
+  }
 `;
 
 export const AccordionSwitch = styled.div`
@@ -107,4 +153,10 @@ export const AccordionSwitch = styled.div`
   transform-origin: center;
   transition: all 0.2s;
   transform: ${({ isOpen }) => isOpen && "scale(1, -1)"};
+  @media (max-width: 768px) {
+    right: 17px;
+    top: 16px;
+    width: 30px;
+    height: 30px;
+  }
 `;
