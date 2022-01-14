b78f4febcec907ddada4cb8ab34a63c686b74c27
Yiriy Vasiliev <yuriy.vasiliev@artmyweb.com>
2022-01-14 17:17:16 +0200

    update privat policy and tems navigation link

--- src/ui/blocks/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/ui/blocks/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
+import { useLocation, useNavigate } from "react-router-dom";
 import {
   Body,
   Content,
@@ -8,25 +10,43 @@ import {
   Title,
 } from "./PrivacyPolicyStyled";
 import { Container } from "../../base/Container";
+import { routesPath } from "../../../conts/routes";
 
 const PrivacyPolicy = () => {
   const [active, setActive] = useState({ policy: true, terms: false });
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { tab } = queryString.parse(location.search);
+
+  const handleSelectPrivacy = () => {
+    setActive({ policy: true, terms: false });
+    navigate(`${routesPath.privacy}?tab=privacy`, { replace: true });
+  };
+
+  const handleSelectTerms = () => {
+    setActive({ policy: false, terms: true });
+    navigate(`${routesPath.privacy}?tab=terms`, { replace: true });
+  };
+
+  useEffect(() => {
+    if (tab === "privacy") {
+      setActive({ policy: true, terms: false });
+    } else {
+      setActive({ policy: false, terms: true });
+    }
+  }, [tab]);
+
   return (
     <PrivacyPolicyWrapper>
       <Container>
         <Title>{active.policy ? "Privacy Policy" : "Terms"}</Title>
         <Body>
           <SideBar>
-            <SideBarItem
-              onClick={() => setActive({ policy: true, terms: false })}
-              active={active.policy}
-            >
+            <SideBarItem onClick={handleSelectPrivacy} active={active.policy}>
               Privacy Policy
             </SideBarItem>
-            <SideBarItem
-              onClick={() => setActive({ policy: false, terms: true })}
-              active={active.terms}
-            >
+            <SideBarItem onClick={handleSelectTerms} active={active.terms}>
               Terms of Use
             </SideBarItem>
           </SideBar>
